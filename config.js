edd0ef7c5517ccbd282ea1666020d1e8ed54076f
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-10 17:40:14 -0600

    Adding configuration

---
+++ config.js
@@ -0,0 +1,10 @@
+const { config } = require('dotenv');
+
+config();
+
+module.exports = {
+    host: process.env.HOST || "",
+    user: process.env.USER || "",
+    password: process.env.PASSWORD || "",
+    database: process.env.DATABASE || ""
+}
--- src/app.js
+++ src/app.js
@@ -3,4 +3,5 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
 app.listen(port, () => console.log());

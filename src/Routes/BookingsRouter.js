f9af071a0626d76269149821da12badc086960f6
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-13 13:06:39 -0600

    Creating Get and delete endpoints

--- src/Routes/BookingsRouter.js
+++ src/Routes/BookingsRouter.js
@@ -2,9 +2,10 @@ const express = require('express');
 const { BookingController } = require('../Controllers/BookingController');
 
 const BookingRoute = express.Router();
-const bookng = new BookingController();
+const booking = new BookingController();
 
-BookingRoute.get(('/'), bookng.getBookings);
+BookingRoute.get(('/'), booking.getBookings);
+BookingRoute.delete(('/:id'), booking.deleteBooking);
 
 module.exports = {
     BookingRoute

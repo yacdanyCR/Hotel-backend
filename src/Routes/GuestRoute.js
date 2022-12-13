d6059e073db36488e6c0a7a0dfca12e36d2de36f
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-13 17:00:54 -0600

    Adding search endpoint

--- src/Routes/GuestRoute.js
+++ src/Routes/GuestRoute.js
@@ -8,6 +8,7 @@ GuestRoute.get(('/'), guest.getAllGuest);
 GuestRoute.post(('/'), guest.addGuest);
 GuestRoute.delete(('/:id'), guest.deleteGuest);
 GuestRoute.put(('/'), guest.updateGuest);
+GuestRoute.get(('/:search'), guest.serchGuest);
 
 module.exports = {
     GuestRoute

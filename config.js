ace9f118217c6011ca04d3290576eb5839bdef4f
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-16 13:06:24 -0600

    Adding port configuration

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ module.exports = {
     host: process.env.HOST || "",
     user: process.env.USER || "",
     password: process.env.PASSWORD || "",
-    database: process.env.DATABASE || ""
+    database: process.env.DATABASE || "",
+    port: process.env.POR || ""
 }
